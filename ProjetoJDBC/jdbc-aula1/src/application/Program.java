d843a09c1ade0930222c62d848172b4b058b854c
RAPHAEL DE LEMOS PIRES <raphaellpires@gmail.com>
2026-04-07 09:07:58 -0300

    No projeto base adicionado o closeResultSet e closeStatement

--- ProjetoJDBC/jdbc-aula1/src/application/Program.java
+++ ProjetoJDBC/jdbc-aula1/src/application/Program.java
@@ -1,15 +1,42 @@
 package application;
 
 import db.DB;
+import db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Program {
 
     static void main(String[] args) {
 
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+
+            rs = st.executeQuery("SELECT * FROM department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + " - " + rs.getString("Name"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+            DB.closeConnection();
+        }
 
     }
 }
